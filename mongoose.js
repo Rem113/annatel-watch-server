133280821c7b3b6bdd63eb53621bf152e0f1c612
Hillel Saal <hillelsaal@kalstudies.com>
2019-11-17 21:43:21 +0200

    Fixed bug

--- mongoose.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://admin:gabhil@88.218.220.20:9000/Annatel?authSource=admin")
-  .then(() => console.log("Connected to MongoDB"));
--- server.js
+++ server.js
@@ -9,3 +9,10 @@ server.listen(port, () => console.log("Listening on port " + port));
 server.on("connection", socket => {
   handleRequests(socket);
 });
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://admin:gabhil@88.218.220.20:9000/Annatel?authSource=admin")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error(err));
