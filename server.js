34c200ee9821787fe375fb4645f6b5feb0253185
Hillel Saal <hillelsaal@kalstudies.com>
2019-11-18 15:36:34 +0200

    Added basic REST API

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const net = require("net");
+const express = require("express");
 const handleRequests = require("./handleRequests");
 
 const server = net.createServer();
-const port = 5088;
+const watchPort = 5088;
 
-server.listen(port, () => console.log("Listening on port " + port));
+server.listen(watchPort, () =>
+  console.log(`Listening for watch on port ${watchPort}`)
+);
 
 server.on("connection", socket => {
   handleRequests(socket);
@@ -16,3 +19,17 @@ mongoose
   .connect("mongodb://admin:gabhil@88.218.220.20:9000/Annatel?authSource=admin")
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error(err));
+
+const app = express();
+const restPort = 8000;
+
+app.listen(restPort, () =>
+  console.log(`Listening for API requests on port ${restPort}`)
+);
+
+const Action = require("./models/Action");
+
+app.get("/", async (req, res) => {
+  const actions = await Action.find();
+  res.end(JSON.stringify(actions));
+});
