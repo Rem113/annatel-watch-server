b10b47038f3ceef1d017f2be78de1338415c4e9c
root <root@watch01-redirector.annatel.net>
2019-11-19 15:34:40 +0200

    Fixed routes

--- api/api.js
+++ api/api.js
@@ -6,4 +6,4 @@ app.listen(restPort, () =>
   console.log(`Listening for API requests on port ${restPort}`)
 );
 
-app.get("/api/action", require("./routes/action"));
+app.use("/api/action", require("./routes/action"));
