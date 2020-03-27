a97be28af8843816c7614e2d32c02f0a653bda6d
Hillel Saal <hillelsaal@kalstudies.com>
2020-03-27 09:43:50 +0100

    Added Date for console.error()

--- mongoose.js
+++ mongoose.js
@@ -7,4 +7,4 @@ module.exports = async () =>
       { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.error(err));
+    .catch(err => console.error(`[${new Date().toLocaleString()}]`, err));
