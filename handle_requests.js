7b38f5feb71530542636c7d1fc2b69ab46bd2e7e
Hillel Saal <remi.saal@gmail.com>
2020-07-08 16:43:10 +0000

    Fixed axios request

--- handle_requests.js
+++ handle_requests.js
@@ -31,12 +31,14 @@ module.exports = (socket) => {
   socket.on("data", async (data) => {
     const action = deserialize(data.toString())
 
-    Axios.post("localhost:3000/api/watch/message", {
-      serial: action.serial,
+    await Axios.post("http://88.218.220.20:3000/api/watch/message", {
+      serial: action.watchId,
       vendor: action.vendor,
-      type: action.type,
+      type: action.actionType,
       length: action.length,
     })
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err))
 
     if (expectReaction.includes(action.actionType))
       respondToAction(action, socket)
